2fba0b060260d3388feb647084dde82cbf60aa67
Maheshivara <gabrimath@gmail.com>
2024-01-12 08:38:12 -0300

    fix: Rename file and classes for better comprehension

--- src/main/java/com/casino_gacha/webapp/WebApp.java
+++ src/main/java/com/casino_gacha/webapp/WebApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebappApplication {
+public class WebApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebappApplication.class, args);
+		SpringApplication.run(WebApp.class, args);
 	}
 
 }
--- src/test/java/com/casino_gacha/webapp/WebAppTests.java
+++ src/test/java/com/casino_gacha/webapp/WebAppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebappApplicationTests {
+class WebAppTests {
 
 	@Test
 	void contextLoads() {
