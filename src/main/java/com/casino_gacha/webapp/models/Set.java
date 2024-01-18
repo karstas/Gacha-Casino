3507f28459d0aede2db81717965cd6a1e028405a
Maheshivara <gabrimath@gmail.com>
2024-01-18 12:10:49 -0300

    fix: removing unused imports

--- src/main/java/com/casino_gacha/webapp/models/Set.java
+++ src/main/java/com/casino_gacha/webapp/models/Set.java
@@ -6,12 +6,8 @@ import java.util.UUID;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
-import com.casino_gacha.webapp.enums.UserRole;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
