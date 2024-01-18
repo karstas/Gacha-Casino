b627cc9fda1d77ade549042bcb6db75da574cbdc
Luan <luancgs@proton.me>
2024-01-18 01:51:49 -0300

    feat: create card image model

---
+++ src/main/java/com/casino_gacha/webapp/models/Image.java
@@ -0,0 +1,128 @@
+package com.casino_gacha.webapp.models;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "image")
+public class Image {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", nullable = false, columnDefinition = "uuid default uuid_generate_v4()")
+  private UUID id;
+
+  @ManyToOne
+  @JoinColumn(name = "card_id", nullable = false)
+  private Card card;
+
+  @Column(name = "artist", nullable = false)
+  private String artist;
+
+  @Column(name = "url", nullable = false, unique = true)
+  private String url;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false)
+  private Timestamp createdAt;
+
+  @Column(name = "created_by")
+  private UUID createdBy;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private Timestamp updatedAt;
+
+  @Column(name = "updated_by")
+  private UUID updatedBy;
+
+  @Column(name = "deleted_at")
+  private Timestamp deletedAt;
+
+  @Column(name = "deleted_by")
+  private UUID deletedBy;
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setArtist(String artist) {
+    this.artist = artist;
+  }
+
+  public String getArtist() {
+    return artist;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setCreatedAt(Timestamp createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Timestamp getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedBy(UUID createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public UUID getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setUpdatedAt(Timestamp updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public Timestamp getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedBy(UUID updatedBy) {
+    this.updatedBy = updatedBy;
+  }
+
+  public UUID getUpdatedBy() {
+    return updatedBy;
+  }
+
+  public void setDeletedAt(Timestamp deletedAt) {
+    this.deletedAt = deletedAt;
+  }
+
+  public Timestamp getDeletedAt() {
+    return deletedAt;
+  }
+
+  public void setDeletedBy(UUID deletedBy) {
+    this.deletedBy = deletedBy;
+  }
+
+  public UUID getDeletedBy() {
+    return deletedBy;
+  }
+}
