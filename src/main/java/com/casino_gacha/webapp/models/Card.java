6b02e0d4937eae017ad11335135095fe6489732c
Luan <luancgs@proton.me>
2024-01-18 01:51:14 -0300

    chore: update card model

--- src/main/java/com/casino_gacha/webapp/models/Card.java
+++ src/main/java/com/casino_gacha/webapp/models/Card.java
@@ -1,18 +1,37 @@
 package com.casino_gacha.webapp.models;
 
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import com.casino_gacha.webapp.enums.CardGender;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "card")
-public class Card extends Base {
+public class Card {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", nullable = false, columnDefinition = "uuid default uuid_generate_v4()")
+  private UUID id;
 
   @Column(name = "name", nullable = false, unique = true)
   private String name;
 
+  @Enumerated(EnumType.STRING)
   @Column(name = "gender", nullable = false)
   private CardGender gender;
 
@@ -24,6 +43,37 @@ public class Card extends Base {
 
   @Column(name = "value", nullable = false)
   private int value;
+
+  @OneToMany(mappedBy = "id")
+  private List<Image> images;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false)
+  private Timestamp createdAt;
+
+  @Column(name = "created_by")
+  private UUID createdBy;
+
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private Timestamp updatedAt;
+
+  @Column(name = "updated_by")
+  private UUID updatedBy;
+
+  @Column(name = "deleted_at")
+  private Timestamp deletedAt;
+
+  @Column(name = "deleted_by")
+  private UUID deletedBy;
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public UUID getId() {
+    return id;
+  }
 
   public void setName(String name) {
     this.name = name;
@@ -63,5 +113,53 @@ public class Card extends Base {
 
   public int getValue() {
     return value;
+  }
+
+  public void setCreatedAt(Timestamp createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Timestamp getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedBy(UUID createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public UUID getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setUpdatedAt(Timestamp updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+  public Timestamp getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedBy(UUID updatedBy) {
+    this.updatedBy = updatedBy;
+  }
+
+  public UUID getUpdatedBy() {
+    return updatedBy;
+  }
+
+  public void setDeletedAt(Timestamp deletedAt) {
+    this.deletedAt = deletedAt;
+  }
+
+  public Timestamp getDeletedAt() {
+    return deletedAt;
+  }
+
+  public void setDeletedBy(UUID deletedBy) {
+    this.deletedBy = deletedBy;
+  }
+
+  public UUID getDeletedBy() {
+    return deletedBy;
   }
 }
