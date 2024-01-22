9b06229b68ffec28fb6a09c648f6076ce99feb41
Maheshivara <gabrimath@gmail.com>
2024-01-22 07:58:25 -0300

    chore: use lombok decorators in casino user PK

--- src/main/java/com/casino_gacha/webapp/models/composite_keys/CasinoUserPK.java
+++ src/main/java/com/casino_gacha/webapp/models/composite_keys/CasinoUserPK.java
@@ -1,7 +1,6 @@
 package com.casino_gacha.webapp.models.composite_keys;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import com.casino_gacha.webapp.models.Casino;
 import com.casino_gacha.webapp.models.User;
@@ -9,7 +8,9 @@ import com.casino_gacha.webapp.models.User;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.Data;
 
+@Data
 @Embeddable
 public class CasinoUserPK implements Serializable {
 
@@ -21,36 +22,4 @@ public class CasinoUserPK implements Serializable {
   @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
-  public void setCasino(Casino casino) {
-    this.casino = casino;
-  }
-
-  public Casino getCasino() {
-    return casino;
-  }
-
-  public void setUser(User user) {
-    this.user = user;
-  }
-
-  public User getUser() {
-    return user;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj)
-      return true;
-    if (obj == null)
-      return false;
-    if (getClass() != obj.getClass())
-      return false;
-    CasinoUserPK other = (CasinoUserPK) obj;
-    return Objects.equals(casino, other.casino) && Objects.equals(user, other.user);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(casino, user);
-  }
 }
