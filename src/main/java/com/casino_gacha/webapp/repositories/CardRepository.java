461d4fdc012a722f9acab63bd617488322f82144
Maheshivara <gabrimath@gmail.com>
2024-01-22 13:37:17 -0300

    chore: edit id type to uuid

--- src/main/java/com/casino_gacha/webapp/repositories/CardRepository.java
+++ src/main/java/com/casino_gacha/webapp/repositories/CardRepository.java
@@ -1,9 +1,11 @@
 package com.casino_gacha.webapp.repositories;
 
+import java.util.UUID;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.casino_gacha.webapp.models.Card;
 
-public interface CardRepository extends CrudRepository<Card, Integer> {
+public interface CardRepository extends CrudRepository<Card, UUID> {
 
 }
